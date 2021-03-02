a88015cb6d6275a5e23797a0fe1b4331aef2815e
KRE <tcheetox@gmail.com>
2021-03-02 13:51:16 +0100

    Enabled :classic autoloader for production

--- config/application.rb
+++ config/application.rb
@@ -21,11 +21,14 @@ module HelpexchangeServer
     config.debug_exception_response_format = :default
     config.load_defaults 6.0
     
-   # config.autoloader = :classic
     config.time_zone = "Europe/Brussels"
     config.active_record.default_timezone = "Europe/Brussels"
 
     config.autoload_paths << "#{Rails.root}/lib/assets"
+
+    if Rails.env.production?
+      config.autoloader = :classic
+    end
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
